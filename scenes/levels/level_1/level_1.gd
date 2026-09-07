1ec03a0a91160fbd88fdb4f46dc607a929edc9ac
ANG YONG XEN <yongxenn@gmail.com>
2026-09-08 02:23:21 +0800

    update level_1

--- scenes/levels/level_1/level_1.gd
+++ scenes/levels/level_1/level_1.gd
@@ -1,17 +1,18 @@
 extends Node2D
-@onready var win_label = $CanvasLayer/WinLabel
+
+@onready var win_panel = $WinCanvasLayer/Panel
 @onready var congratulations_sound = $CongratulationsSound
 
 func level_complete():
-	win_label.visible = true
-	win_label.scale = Vector2(0, 0)
+	win_panel.visible = true
+	win_panel.scale = Vector2(0, 0)
 	congratulations_sound.play()
 
 	var tween = create_tween()
 	tween.set_trans(Tween.TRANS_BACK)
 	tween.set_ease(Tween.EASE_OUT)
-	tween.tween_property(win_label, "scale", Vector2(1, 1), 0.5)
+	tween.tween_property(win_panel, "scale", Vector2(1, 1), 0.5)
 
-	await get_tree().create_timer(3.0).timeout
+	await get_tree().create_timer(2.0).timeout
 
 	get_tree().change_scene_to_file("res://scenes/ui/level_select.tscn")
