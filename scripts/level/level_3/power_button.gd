f847c8d43a64f6a0a6f6fc558b305a35a277c17e
Siumun <siumunsiumun5@gmail.com>
2026-09-17 12:36:23 +0800

    Remove dead code in level 3

--- scripts/level/level_3/power_button.gd
+++ scripts/level/level_3/power_button.gd
@@ -1,12 +1,11 @@
 extends TextureButton
 
 func _ready() -> void:
-	print(">>> PowerButton 的 _ready 执行了 <<<")
 	pressed.connect(_on_self_pressed)
 
 func _on_self_pressed() -> void:
-	print(">>> PowerButton 自己检测到被点击了 <<<")
+	print(">>> PowerButton self pressed <<<")
 
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		print(">>> PowerButton 收到鼠标事件: ", event.pressed, " at ", event.position)
+		print(">>> PowerButton: ", event.pressed, " at ", event.position)
